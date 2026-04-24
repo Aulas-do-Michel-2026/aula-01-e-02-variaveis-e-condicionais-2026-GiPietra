5241715282927e71038f27f1b192dc7b5b7f319b
GiPietra <giuliapietra.nascimento@einstein.edu.br>
2026-04-24 10:43:52 -0300

    Update exercicio_2.py

--- exercicio_2.py
+++ exercicio_2.py
@@ -13,3 +13,9 @@ Digite o peso do paciente (em kg):
 Resposta:
 Média: 140 mg
 """
+
+Peso = input("Digite o seu peso em Kg:")
+
+Dosagem = int(Peso) * 2
+
+print(f"Sua dosagem será de {Dosagem} mg") 
